58c56175a757d32ad956871bdad71c0fc91bb9e1
Karol Burczyński <karobur@statoilfuelretail.com>
2019-07-28 23:24:50 +0200

    Fixed not defined version + code cleaning

--- src/main/java/karklik/net/http/SimpleBodyHandlerValidator.java
+++ src/main/java/karklik/net/http/SimpleBodyHandlerValidator.java
@@ -4,13 +4,12 @@ import lombok.Builder;
 import lombok.NonNull;
 import lombok.Singular;
 
-import java.io.IOException;
-import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 @Builder(builderMethodName = "newBuilder")
 public class SimpleBodyHandlerValidator<T> implements HttpResponse.BodyHandler<T> {
@@ -31,59 +30,54 @@ public class SimpleBodyHandlerValidator<T> implements HttpResponse.BodyHandler<T
     @Override
     public HttpResponse.BodySubscriber<T> apply(HttpResponse.ResponseInfo responseInfo) {
         Optional.of(allowedStatusCodes)
-                .filter(integers -> !integers.isEmpty())
-                .ifPresent(integers -> integers.stream()
-                        .filter(value -> value == responseInfo.statusCode())
-                        .findAny()
-                        .orElseThrow(() ->
-                                new HttpResponseInfoStatusCodeException("Received status code: "
-                                        + responseInfo.statusCode()
-                                        + ", Allowed status codes: "
-                                        + allowedStatusCodes)));
+                .filter(codes -> !codes.isEmpty())
+                .ifPresent(integers -> checkStatusCode(responseInfo));
         // TODO: Add checking for exact requiredHeader values instead of comparing full header
         Optional.of(requiredHeaders)
-                .filter(map -> !map.isEmpty())
-                .ifPresent(map -> map.entrySet()
+                .filter(headers -> !headers.isEmpty())
+                .ifPresent(headers -> checkHeaders(responseInfo));
+        if (expectedVersion != null)
+            checkVersion(responseInfo);
+        return handler.apply(responseInfo);
+    }
+
+
+    private void checkHeaders(HttpResponse.ResponseInfo responseInfo) {
+        requiredHeaders.entrySet()
+                .parallelStream()
+                .forEach(requiredHeader -> responseInfo.headers()
+                        .map()
+                        .entrySet()
                         .parallelStream()
-                        .forEach(requiredHeader -> responseInfo.headers()
-                                .map()
-                                .entrySet()
-                                .parallelStream()
-                                .filter(header -> requiredHeader.getKey().equalsIgnoreCase(header.getKey())
-                                        && requiredHeader.getValue().equals(header.getValue()))
-                                .findAny()
-                                .orElseThrow(() ->
-                                        new HttpResponseInfoHeaderException("Missing requiredHeader: "
-                                                + requiredHeader
-                                                + ", Received headers: "
-                                                + responseInfo.headers()
-                                                + ", Expected headers: "
-                                                + requiredHeaders))));
-        Optional.ofNullable(expectedVersion)
-                .filter(v -> responseInfo.version().equals(v))
+                        .filter(header -> requiredHeader.getKey().equalsIgnoreCase(header.getKey())
+                                && requiredHeader.getValue().equals(header.getValue()))
+                        .findAny()
+                        .orElseThrow(() ->
+                                new HttpResponseInfoHeaderException("Missing requiredHeader: "
+                                        + requiredHeader
+                                        + ", Received headers: "
+                                        + responseInfo.headers()
+                                        + ", Expected headers: "
+                                        + requiredHeaders)));
+    }
+
+    private void checkStatusCode(HttpResponse.ResponseInfo responseInfo) {
+        allowedStatusCodes.stream()
+                .filter(value -> value == responseInfo.statusCode())
+                .findAny()
+                .orElseThrow(() ->
+                        new HttpResponseInfoStatusCodeException("Received status code: "
+                                + responseInfo.statusCode()
+                                + ", Allowed status codes: "
+                                + allowedStatusCodes));
+    }
+
+    private void checkVersion(HttpResponse.ResponseInfo responseInfo) {
+        Optional.of(expectedVersion)
+                .filter(version -> responseInfo.version().equals(version))
                 .orElseThrow(() -> new HttpResponseInfoVersionException("Received response version: "
                         + responseInfo.version()
                         + ", Expected response version: "
                         + expectedVersion));
-        return handler.apply(responseInfo);
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_2)
-                .followRedirects(HttpClient.Redirect.ALWAYS)
-                .connectTimeout(Duration.ofMillis(30000))
-                .build();
-        HttpRequest request = HttpRequest.newBuilder(URI.create("https://github.com"))
-                .header("Content-Type", "text/html")
-                .GET()
-                .build();
-        HttpResponse response = client.send(request, responseInfo ->
-                SimpleBodyHandlerValidator.newBuilder(HttpResponse.BodyHandlers.discarding())
-                        .allowedStatusCode(200)
-                        .requiredHeader("Content-Type",
-                                Collections.singletonList("text/html; charset=utf-8"))
-                        .build()
-                        .apply(responseInfo));
     }
 }
